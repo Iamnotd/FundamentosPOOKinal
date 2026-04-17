088af0afc80dff8b8ba1aeee01e5d3f57ed72b04
Derek Gonzalez <dgonzalez-2024486@kinal.edu.gt>
2026-04-17 11:26:34 -0600

    000 Primer commit del proyecto

---
+++ src/org/derekgonzalez/system/Principal.java
@@ -0,0 +1,11 @@
+package org.derekgonzalez.system;
+
+/**
+ *
+ * @author derekgonzalez
+ */
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo estoy n kinal");
+    }
+}
